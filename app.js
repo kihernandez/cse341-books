66d5eea3fa9754e8067c011c3207e092c85ebb13
kihernandez <khernandezeuceda@byupathway.edu>
2026-09-10 22:54:44 -0500

    Implemented error handling for undefined routes

--- app.js
+++ app.js
@@ -10,4 +10,11 @@ app.get('/', (req, res) => {
   return res.status(200).json({ message: 'Server is running' });
 });
 
+
+app.use((req, res) => {
+  return res.status(404).json({
+    message: 'Route not found'
+  });
+});
+
 export default app;
