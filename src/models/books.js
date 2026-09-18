aec243a3e1c48900288dbcf16d66545173a025fe
kihernandez <khernandezeuceda@byupathway.edu>
2026-09-17 22:12:53 -0500

    Implement book API with author  references

--- src/models/books.js
+++ src/models/books.js
@@ -4,6 +4,7 @@ const getAllBooks = async () => {
   const db = getDb();
   const collection = db.collection('books');
   const books = await collection.find({}).toArray();
+
   return books;
 };
 
@@ -11,7 +12,53 @@ const getBookById = async (bookId) => {
   const db = getDb();
   const collection = db.collection('books');
   const book = await collection.findOne({ id: bookId });
+
   return book;
 };
 
-export { getAllBooks, getBookById };
+const createBook = async (book) => {
+  const db = getDb();
+  const collection = db.collection('books');
+  await collection.insertOne(book);
+
+  return book;
+};
+
+const updateBook = async (bookId, book) => {
+  const db = getDb();
+  const collection = db.collection('books');
+
+  await collection.updateOne(
+    { id: bookId },
+    { $set: book }
+  );
+
+  return { id: bookId, ...book };
+};
+
+const deleteBook = async (bookId) => {
+  const db = getDb();
+  const collection = db.collection('books');
+
+  const result = await collection.deleteOne({ id: bookId });
+
+  return result;
+};
+
+const authorExists = async (authorId) => {
+  const db = getDb();
+  const collection = db.collection('authors');
+
+  const author = await collection.findOne({ id: authorId });
+
+  return !!author;
+};
+
+export {
+  getAllBooks,
+  getBookById,
+  createBook,
+  updateBook,
+  deleteBook,
+  authorExists
+};
